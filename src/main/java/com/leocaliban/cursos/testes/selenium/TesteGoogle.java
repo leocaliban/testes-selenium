32c25205101a32fda07dcc4d0a79e3d0e40986e7
Leocaliban <leocaliban@gmail.com>
2018-04-24 20:51:54 -0300

    Testa com assertiva JUnit

--- src/main/java/com/leocaliban/cursos/testes/selenium/TesteGoogle.java
+++ src/main/java/com/leocaliban/cursos/testes/selenium/TesteGoogle.java
@@ -1,16 +1,19 @@
 package com.leocaliban.cursos.testes.selenium;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteGoogle {
 
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
 		/** Se a variavel de ambiente não for setada, é nescessário essa linha
 			System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
 		*/
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
-		System.out.println(driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
 	}
 }
