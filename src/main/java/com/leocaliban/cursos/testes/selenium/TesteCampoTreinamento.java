752b2390908874b8dd76e6b292abf875187bd69c
Leocaliban <leocaliban@gmail.com>
2018-04-24 22:44:06 -0300

    Teste de elementos básicos [TextField e TextArea]

--- src/main/java/com/leocaliban/cursos/testes/selenium/TesteCampoTreinamento.java
+++ src/main/java/com/leocaliban/cursos/testes/selenium/TesteCampoTreinamento.java
@@ -1,6 +1,8 @@
 package com.leocaliban.cursos.testes.selenium;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
@@ -9,15 +11,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TesteCampoTreinamento {
 	
 	@Test
-	public void teste() {
+	public void testeTextField() {
 		WebDriver driver = new ChromeDriver();
 
 		driver.manage().window().setPosition(new Point(50, 50));
 		driver.manage().window().setSize(new Dimension(1080, 500));
 		
-		driver.get("file:///" + System.getProperty("user.id") + "/src/main/resources/campo-treinamento/componentes.html");
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/campo-treinamento/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Escrever texto para teste");
+		
+		Assert.assertEquals("Escrever texto para teste", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 		
 		driver.quit();
 	}
 
+	@Test
+	public void deveInteragirComTextArea() {
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().setPosition(new Point(50, 50));
+		driver.manage().window().setSize(new Dimension(1080, 500));
+		
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/campo-treinamento/componentes.html");
+		
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Escrever texto para teste");
+		
+		Assert.assertEquals("Escrever texto para teste", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+		
+		driver.quit();
+	}
+	
 }
