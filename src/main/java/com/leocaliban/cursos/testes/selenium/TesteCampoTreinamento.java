078b07a25b45703bccf581fedad15d7d68099c9b
Leocaliban <leocaliban@gmail.com>
2018-04-24 21:32:44 -0300

    Cria TesteCampoTreinamento [Local]

---
+++ src/main/java/com/leocaliban/cursos/testes/selenium/TesteCampoTreinamento.java
@@ -0,0 +1,23 @@
+package com.leocaliban.cursos.testes.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteCampoTreinamento {
+	
+	@Test
+	public void teste() {
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().setPosition(new Point(50, 50));
+		driver.manage().window().setSize(new Dimension(1080, 500));
+		
+		driver.get("file:///" + System.getProperty("user.id") + "/src/main/resources/campo-treinamento/componentes.html");
+		
+		driver.quit();
+	}
+
+}
